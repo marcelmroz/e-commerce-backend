f65faa025b117dcce60207cf33400cc91d408fe9
Marcel Mroz <mmroz.marcel@gmail.com>
2023-10-28 11:25:41 +0200

    Customer entity update

--- src/main/java/com/example/ecommercebackend/entity/Customer.java
+++ src/main/java/com/example/ecommercebackend/entity/Customer.java
@@ -1,4 +1,48 @@
 package com.example.ecommercebackend.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "customer")
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_address")
+    private String emailAddress;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "date_of_birth")
+    private Date dateOfBirth;
+
+
+
+
+//    Address (with sub-parameters: Street, City, State, Zip Code, Country)
+//    Order History
+//    Account Status
+
 }
