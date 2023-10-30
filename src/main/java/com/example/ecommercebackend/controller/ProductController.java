bcc3dd1e7dfa02d72920d253526453a303ce7845
Marcel Mroz <mmroz.marcel@gmail.com>
2023-10-30 16:02:35 +0100

    working GET by id method

--- src/main/java/com/example/ecommercebackend/controller/ProductController.java
+++ src/main/java/com/example/ecommercebackend/controller/ProductController.java
@@ -5,10 +5,7 @@ import com.example.ecommercebackend.service.ProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
@@ -22,6 +19,13 @@ public class ProductController {
     public ResponseEntity<ProductDto> createProduct(@RequestBody ProductDto productDto){
         ProductDto savedProduct = productService.createProduct(productDto);
         return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+    }
+
+//    //Build Get Product by id REST API
+    @GetMapping("{id}")
+    public ResponseEntity<ProductDto> getProductById(@PathVariable("id") Long productId){
+        ProductDto productDto = productService.getProductById(productId);
+        return ResponseEntity.ok(productDto);
     }
 
 }
