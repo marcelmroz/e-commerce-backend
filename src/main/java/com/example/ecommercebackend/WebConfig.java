637ead582554bb545d3699a8a26c7257ef0ef6f7
Marcel Mroz <mmroz.marcel@gmail.com>
2023-11-11 20:06:53 +0100

    added webConfig

---
+++ src/main/java/com/example/ecommercebackend/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.ecommercebackend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply to all endpoints in the application
+                .allowedOrigins("http://localhost:3000") // Allow this origin to make requests
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow these HTTP methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(true); // Allow credentials like cookies, authorization headers, etc.
+    }
+
+}
