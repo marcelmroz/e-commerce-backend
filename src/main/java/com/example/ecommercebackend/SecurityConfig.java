04478c8f5fb16807c48985fed67e2fcc3a3fe801
Marcel Mroz <mmroz.marcel@gmail.com>
2023-11-28 15:40:02 +0100

    working login with spring secure

--- src/main/java/com/example/ecommercebackend/SecurityConfig.java
+++ src/main/java/com/example/ecommercebackend/SecurityConfig.java
@@ -23,12 +23,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf.disable()) //TO DO
+                .formLogin(form -> form.loginPage("/login").permitAll())
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/**").permitAll()
+                        .requestMatchers("/api/customers/*").permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults());
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
