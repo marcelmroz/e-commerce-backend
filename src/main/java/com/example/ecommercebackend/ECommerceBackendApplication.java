e7f5c69e9ebc75c24ae720586f38a4212d405eb3
Marcel Mroz <mmroz.marcel@gmail.com>
2023-11-28 10:32:25 +0100

    enable spring security + can't POST login endpoint

--- src/main/java/com/example/ecommercebackend/ECommerceBackendApplication.java
+++ src/main/java/com/example/ecommercebackend/ECommerceBackendApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)public class ECommerceBackendApplication {
+@SpringBootApplication
+//		(exclude = SecurityAutoConfiguration.class)
+public class ECommerceBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceBackendApplication.class, args);
 	}
---
+++ src/main/java/com/example/ecommercebackend/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.example.ecommercebackend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+}
